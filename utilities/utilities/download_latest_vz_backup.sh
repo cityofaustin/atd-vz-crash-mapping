22701c2f025bddf8808a0d888652c4179b67f7a4
Frank Hereford <frankhereford@users.noreply.github.com>
2022-01-31 15:27:11 -0600

    remove extra linefeed

--- utilities/utilities/download_latest_vz_backup.sh
+++ utilities/utilities/download_latest_vz_backup.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 aws s3 cp s3://atd-vision-zero-database/backups/atd_vz_data_production/public/`date  --date="yesterday" +"%Y-%m-%d"`/atd_vz_data-full.sql.gz /root/tmp/atd_vz_data-full.sql.gz
-
