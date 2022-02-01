75ba9a859cd1ca8a2e25ba2d323ca46d0135a548
Frank Hereford <frankhereford@users.noreply.github.com>
2022-02-01 10:58:50 -0600

    Capture diagram data from existing DB prior to reload

--- utilities/utilities/pull_vz_backup_populate_local_db.sh
+++ utilities/utilities/pull_vz_backup_populate_local_db.sh
@@ -2,9 +2,10 @@
 
 /root/utilities/download_latest_vz_backup.sh
 
-echo 'drop database atd_vz_data' | psql -U $POSTGRES_USER -h $POSTGRES_HOSTNAME $POSTGRES_DB
-echo 'create database atd_vz_data' | psql -U $POSTGRES_USER -h $POSTGRES_HOSTNAME $POSTGRES_DB
-echo 'create extension postgis' | psql -U $POSTGRES_USER -h $POSTGRES_HOSTNAME $POSTGRES_DB
-echo 'create extension plperl' | psql -U $POSTGRES_USER -h $POSTGRES_HOSTNAME $POSTGRES_DB
+DATE=$(date +"%Y-%m-%d-%H-%M")
+
+echo "Dumping out diagram data tables from DB."
+pg_dump --no-owner -h $POSTGRES_HOSTNAME -U $POSTGRES_USER -t diagram_aoi -t diagram_crashes -t diagram_icon_crash_map -t diagram_intersections -t diagram_sessions $POSTGRES_DB > /root/diagram_data/diagram_$DATE.sql
+
 
 zcat /root/tmp/atd_vz_data-full.sql.gz | /root/utilities/skip_changelog_out_of_db_archive.pl | psql -U $POSTGRES_USER -h $POSTGRES_HOSTNAME $POSTGRES_DB
