671da7ceb3f40058defa3bc5514c4a245cb21d30
Frank Hereford <frankhereford@users.noreply.github.com>
2022-02-01 19:14:30 -0600

    DB reload script that preserves diagram data

--- utilities/utilities/pull_vz_backup_populate_local_db.sh
+++ utilities/utilities/pull_vz_backup_populate_local_db.sh
@@ -15,3 +15,7 @@ echo 'create extension postgis' | psql -U $POSTGRES_USER -h $POSTGRES_HOSTNAME $
 echo 'create extension plperl' | psql -U $POSTGRES_USER -h $POSTGRES_HOSTNAME $POSTGRES_DB
 
 zcat /root/tmp/atd_vz_data-full.sql.gz | /root/utilities/skip_changelog_out_of_db_archive.pl | psql -U $POSTGRES_USER -h $POSTGRES_HOSTNAME $POSTGRES_DB
+
+cat /root/utilities/db_schema/functions.sql | psql -U $POSTGRES_USER -h $POSTGRES_HOSTNAME $POSTGRES_DB 
+cat /root/diagram_data/diagram_$DATE.sql | psql -U $POSTGRES_USER -h $POSTGRES_HOSTNAME $POSTGRES_DB 
+cat /root/utilities/view_materialized_views.sql | psql -U $POSTGRES_USER -h $POSTGRES_HOSTNAME $POSTGRES_DB 
