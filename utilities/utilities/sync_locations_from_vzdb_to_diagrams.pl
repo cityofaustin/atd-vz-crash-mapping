e72e1a1714df1d070ae110e00d21c64514546049
Frank Hereford <frankhereford@users.noreply.github.com>
2022-02-01 19:22:07 -0600

    pulling in qa/qc data sync script

---
+++ utilities/utilities/sync_locations_from_vzdb_to_diagrams.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+
+use strict;
+use JSON;
+use CGI;
+use Data::Dumper;
+use DBI;
+
+my $pg_dts  = DBI->connect("DBI:Pg:dbname=" . $ENV{'DTS_RR_DB'} . ";host=" . $ENV{'DTS_RR_DB_HOST'} . ";port=" . $ENV{'DTS_RR_DB_PORT'}, $ENV{'DTS_RR_DB_USERNAME'}, $ENV{'DTS_RR_DB_PASSWORD'}, {RaiseError => 1});
+my $pg  = DBI->connect("DBI:Pg:dbname=" . $ENV{'LOCAL_DEV_DB'} . ";host=" . $ENV{'LOCAL_DEV_DB_HOST'} . ";port=" . $ENV{'LOCAL_DEV_DB_PORT'}, $ENV{'LOCAL_DEV_DB_USERNAME'}, $ENV{'LOCAL_DEV_DB_PASSWORD'}, {RaiseError => 1});
+
+my $sql = "select crash_id, latitude_primary, longitude_primary, ST_AsText(position) as location from atd_txdot_crashes where position is not null order by crash_date desc";
+my $query = $pg_dts->prepare($sql);
+$query->execute();
+while (my $crash = $query->fetchrow_hashref)
+  {
+  #print Dumper $crash;
+
+  my $sql = "select crash_id, ST_Distance(ST_Transform(position, 2277), ST_Transform(ST_GeomFromText(?, 4326), 2277)) as distance_feet from atd_txdot_crashes where crash_id = ? and ST_AsText(position) != ?";
+  my $query = $pg->prepare($sql);
+  $query->execute($crash->{'location'}, $crash->{'crash_id'}, $crash->{'location'});
+  my $local_crash = $query->fetchrow_hashref;
+
+  next unless $local_crash->{'crash_id'};
+
+  #print $local_crash->{'crash_id'}, " ", $local_crash->{'distance_feet'}, "\n" if $local_crash->{'crash_id'};
+  #print Dumper $local_crash if $local_crash->{'crash_id'};
+
+  next unless $local_crash->{'distance_feet'} > 1;
+
+  my $sql = "update atd_txdot_crashes set latitude_primary = ?, longitude_primary = ?, position = ST_GeomFromText(?, 4326) where crash_id = ?";
+  my $query = $pg->prepare($sql);
+  $query->execute($crash->{'latitude_primary'}, $crash->{'longitude_primary'}, $crash->{'location'}, $crash->{'crash_id'});
+  print "Got one! - ", $local_crash->{'crash_id'}, " ", $local_crash->{'distance_feet'}, "\n";
+  }
